95918f8278c3fb84fda79f7ae0c97c0d30807a5d
Noahable-Offcial <Noahable.yt@gmail.com>
2025-10-27 22:37:01 +0000

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,18 @@
-// components/Navbar.js
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav style={{
-      display: "flex",
-      justifyContent: "space-around",
-      backgroundColor: "#111",
-      color: "white",
-      padding: "10px"
-    }}>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        backgroundColor: "#111",
+        color: "white",
+        padding: "10px",
+      }}
+    >
       <Link href="/">Home</Link>
       <Link href="/upload">Upload</Link>
-      <Link href="/profile">Profile</Link>
     </nav>
   );
 }
